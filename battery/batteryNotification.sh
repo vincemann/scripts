f2a1b2ead560566d5cc5408dd9b354eef045a225
root <vince@debian.local>
2020-08-19 22:14:40 +0200

    remove and add scripts + fix bug in battery script, always expecting two char wide numbers for curr battery level

--- battery/batteryNotification.sh
+++ battery/batteryNotification.sh
@@ -8,16 +8,22 @@
 # Extracts information from command 'acpi'
 # Checks status of battery by comparing it to given values.
 # Sends notification via 'notify-send'
+
 # dependencies:
+# acpi
 # libnotify-bin on Debian 
 
 status=$(acpi | cut -f 3 -d ' ' | rev | cut -c 2- | rev)
 discharging="Discharging"
 
-current=$(acpi | cut -d " " -f 4 | cut -c 1-2)
+current=$(acpi | cut -d " " -f 4 | sed 's/.$//')
+printf "current battery level: %s\n" "$current"
 minimal=15
 
 if [ "$current" -lt "$minimal" ] && [ "$status" = "$discharging" ]
 then
-	$(notify-send "Battery low")
+	# image not supported by i3 but may be by other window managers
+	notify-send -i low-battery.png "Battery low"
+else
+	printf "battery is still full enough and/or charging\n"
 fi
